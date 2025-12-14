51feb0c2c3f5f1e4fd870ecbbb1d6a81d9899340
Omar Sabry <morasabri422@gmail.com>
2025-12-14 20:13:29 +0200

    redis config

--- config/redis.go
+++ config/redis.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"log"
 	"os"
 
 	"github.com/redis/go-redis/v9"
@@ -11,11 +12,17 @@ func NewRedisClient() *redis.Client {
 	// redisHost := os.Getenv("REDIS_HOST")
 	// redisPort := os.Getenv("REDIS_PORT")
 	redisPassword := os.Getenv("REDIS_PASSWORD")
-	redisUrl := os.Getenv("REDIS_URL")
+	// redisUrl := os.Getenv("REDIS_URL")
+	// log.Printf("DEBUG redisulr %v" , redisUrl)
+	// log.Printf("DEBUG redisPassword %v" , redisPassword)
+	redisHost:= os.Getenv("REDISHOST")
+	redisPort:= os.Getenv("REDISPORT")
+	log.Printf("DEBUG redisHost %v" , redisHost)
+	log.Printf("DEBUG redisPort %v" , redisPort)
 	// addr := redisHost + ":" + redisPort
 
 	return redis.NewClient(&redis.Options{
-		Addr:     redisUrl,
+		Addr:     redisHost + ":" + redisPort,
 		Password: redisPassword, // add if you set one
 		DB:       0,
 		
