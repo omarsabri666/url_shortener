80d8b60f9c72371b09642f9d364f2adcb0b19bab
Omar Sabry <morasabri422@gmail.com>
2025-12-14 04:32:35 +0200

    edit config

--- config/redis.go
+++ config/redis.go
@@ -10,11 +10,13 @@ func NewRedisClient() *redis.Client {
 	LoadEnv()
 	redisHost := os.Getenv("REDIS_HOST")
 	redisPort := os.Getenv("REDIS_PORT")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
 	addr := redisHost + ":" + redisPort
 
 	return redis.NewClient(&redis.Options{
 		Addr:     addr,
-		Password: "", // add if you set one
+		Password: redisPassword, // add if you set one
 		DB:       0,
+		
 	})
 }
